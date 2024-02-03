f56bd239836c3f9210a63f513ea90204fcd9b2ca
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-02-03 18:00:58 +0100

    Init color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       colors: {
         blue: "#0761E9",
+        grey: "rgb(39, 40, 48)",
       },
       fontFamily: {
         headline: ['"Raleway"', "serif"],
