c1b6dbc743565f86478297a24baffc01056338a5
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-02-03 17:53:26 +0100

    Nav structure

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,5 +1,14 @@
 <script setup></script>
 
 <template>
-  <nav></nav>
+  <nav>
+    <div class="nav-inner">
+      <ul>
+        <li><a>Start</a></li>
+        <li><a>Information</a></li>
+        <li><a>Merkmale</a></li>
+        <li><a>Skills</a></li>
+      </ul>
+    </div>
+  </nav>
 </template>
