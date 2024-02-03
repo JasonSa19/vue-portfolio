fb83c090429e81f19697a2dd2fb0c7bf8c74fa9f
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-02-03 16:01:00 +0100

    Add scroll-animation

--- src/main.js
+++ src/main.js
@@ -3,3 +3,17 @@ import App from "./App.vue";
 import "./assets/scss/style.scss";
 
 createApp(App).mount("#app");
+
+// Scroll-Animation
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target); // Stop observing once the element is animated
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".anim");
+hiddenElements.forEach((el) => observer.observe(el));
