926e9af027f3353234e6ccb7e037897d10422091
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-02-03 17:42:32 +0100

    Fix menu toggler

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,35 +17,37 @@ const toggleHeader = () => {
 </script>
 
 <template>
-  <header class="anim container mx-auto mt-11 flex items-center font-light">
-    <vectorLink
-      class="flex flex-1 justify-start"
-      :link="'mailto:' + mailAddress"
-      :buttonText="mailAddress"
-    />
-    <span class="flex flex-1 justify-center gap-1"
-      ><a :href="linkedIn">LinkedIn</a> / <a :href="gitHub">Github</a> /
-      <a :href="instaGram">Instagram</a>
-    </span>
-    <customBtn
-      :link="resume"
-      class="flex flex-1 justify-end"
-      buttonText="Lebenslauf"
-    />
-    <button
-      @click="toggleHeader"
-      :class="{ open: isHeaderOpen }"
-      class="menu-toggle align-end group relative ml-20 flex h-6 w-11 flex-col justify-between transition-all"
-    >
-      <span
-        class="absolute right-0 top-0 inline-block h-[2px] w-3/4 bg-white transition-all group-hover:w-full"
-      ></span>
-      <span
-        class="absolute right-0 top-2/4 inline-block h-[2px] w-full -translate-y-2/4 bg-white"
-      ></span>
-      <span
-        class="absolute bottom-0 right-0 inline-block h-[2px] w-2/4 bg-white transition-all group-hover:w-full"
-      ></span>
-    </button>
+  <header class="anim container mx-auto mt-11 font-light">
+    <div class="header-inner flex items-center" :class="{ open: isHeaderOpen }">
+      <vectorLink
+        class="flex flex-1 justify-start"
+        :link="'mailto:' + mailAddress"
+        :buttonText="mailAddress"
+      />
+      <span class="flex flex-1 justify-center gap-1"
+        ><a :href="linkedIn">LinkedIn</a> / <a :href="gitHub">Github</a> /
+        <a :href="instaGram">Instagram</a>
+      </span>
+      <customBtn
+        :link="resume"
+        class="flex flex-1 justify-end"
+        buttonText="Lebenslauf"
+      />
+      <button
+        @click="toggleHeader"
+        :class="{ open: isHeaderOpen }"
+        class="menu-toggle align-end group relative ml-20 flex h-6 w-11 flex-col justify-between transition-all"
+      >
+        <span
+          class="absolute right-0 top-0 inline-block h-[2px] w-3/4 bg-white transition-all group-hover:w-full"
+        ></span>
+        <span
+          class="absolute right-0 top-2/4 inline-block h-[2px] w-full -translate-y-2/4 bg-white"
+        ></span>
+        <span
+          class="absolute bottom-0 right-0 inline-block h-[2px] w-2/4 bg-white transition-all group-hover:w-full"
+        ></span>
+      </button>
+    </div>
   </header>
 </template>
