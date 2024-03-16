095e80e446bab989fc7d64405fdeaef4524c7724
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-03-16 01:42:20 +0100

    Changes to site-nav

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,13 +1,31 @@
 <template>
   <nav
-    class="absolute items-center justify-center w-full transition-all duration-200 -top-24 bg-grey"
+    class="absolute flex items-center justify-center w-full pt-2 transition-all duration-200 border-b-2 -top-64 bg-grey pb-11 border-blue"
   >
     <div class="nav-inner">
       <ul class="flex justify-center gap-16 xl:text-lg">
-        <li><a href="/">Start</a></li>
-        <li><a href="#info">Information</a></li>
-        <li><a href="#merkmale">Merkmale</a></li>
-        <li><a href="#skills">Skills</a></li>
+        <li>
+          <a class="transition-all duration-200 hover:text-blue" href="/"
+            >Start</a
+          >
+        </li>
+        <li>
+          <a class="transition-all duration-200 hover:text-blue" href="#info"
+            >Information</a
+          >
+        </li>
+        <li>
+          <a
+            class="transition-all duration-200 hover:text-blue"
+            href="#merkmale"
+            >Merkmale</a
+          >
+        </li>
+        <li>
+          <a class="transition-all duration-200 hover:text-blue" href="#skills"
+            >Skills</a
+          >
+        </li>
       </ul>
     </div>
   </nav>
