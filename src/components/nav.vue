9bfc2845551481dde07a64fea0d191921da65644
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-03-21 23:16:05 +0100

    Changes to responsive menu

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,9 +1,9 @@
 <template>
   <nav
-    class="absolute flex flex-col items-center justify-center w-full pt-2 transition-all duration-200 border-b-2 xl:flex-row -top-64 bg-grey pb-11 border-blue"
+    class="absolute z-10 flex items-center justify-center w-full pt-2 transition-all duration-200 border-b-2 -top-[30rem] bg-grey pb-11 border-blue"
   >
-    <div class="ml-4 mr-auto nav-inner xl:ml-0">
-      <ul class="flex justify-center gap-16 xl:text-md">
+    <div class="ml-4 mr-auto nav-inner lg:ml-auto">
+      <ul class="flex flex-col justify-center gap-16 lg:flex-row xl:text-md">
         <li>
           <a class="transition-all duration-200 hover:text-blue" href="/"
             >Start</a
