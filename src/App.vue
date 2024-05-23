a56675cbd45c63ed7315ff94342f2e14330830e6
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-05-23 20:50:13 +0200

    Trial and Error title tag in app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Header from "./components/Header.vue";
-import Intro from "./components/intro.vue";
+import Intro from "./components/Intro.vue";
 import About from "./components/About.vue";
 </script>
 
