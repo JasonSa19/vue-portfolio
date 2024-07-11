623de8948ec4c3e11cfd85508ed0225132c7fbbc
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-07-11 20:14:12 +0200

    Implement svg for area teasers

--- src/components/modules/AreaTeaser.vue
+++ src/components/modules/AreaTeaser.vue
@@ -3,17 +3,25 @@ const props = defineProps({
   headlineText: { type: String },
   flowText: { type: String },
   imageLink: { type: String },
+  svgContent: { type: String },
 });
 </script>
 
 <template>
-  <div class="p-4 basis-full sm:basis-2/4 xl:basis-1/4">
+  <div class="basis-full sm:basis-2/4 xl:basis-1/3">
     <div
-      class="relative py-12 pr-12 bg-center bg-no-repeat bg-cover rounded-2xl after:rounded-2xl pl-11 after:absolute after:top-0 after:right-0 after:z-10 after:left-0 after:bottom-0 after:h-full after:w-full after:bg-teasergrey after:opacity-55"
+      class="relative xl:pt-40 pt-12 pb-12 pr-12 xl:min-h-[680px] bg-center bg-no-repeat bg-cover xl:pb-60 rounded-2xl after:rounded-2xl pl-11 after:absolute after:top-0 after:right-0 after:z-10 after:left-0 after:bottom-0 after:h-full after:w-full after:bg-teasergrey after:opacity-55"
       :style="{ backgroundImage: 'url(' + imageLink + ')' }"
     >
       <div class="flex flex-col items-center justify-center text-center">
-        <h3 class="z-20 mb-2 font-bold font-headline">{{ headlineText }}</h3>
+        <div
+          class="z-20 flex items-center justify-center w-24 h-24 rounded-full xl:w-44 xl:h-44 bg-blue"
+          v-html="svgContent"
+        ></div>
+        <h3
+          class="z-20 mt-5 mb-2 font-medium text-center xl:text-lg font-headline"
+          v-html="headlineText"
+        ></h3>
         <div class="z-20 invisible h-0 opacity-0">{{ flowText }}</div>
       </div>
     </div>
