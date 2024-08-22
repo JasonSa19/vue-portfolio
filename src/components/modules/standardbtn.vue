504a41fd509cdf5d6d2730b350110448b951a205
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-08-22 11:13:54 +0200

    Change standard-btn

--- src/components/modules/standardbtn.vue
+++ src/components/modules/standardbtn.vue
@@ -10,13 +10,19 @@ const props = defineProps({
 <template>
   <div class="btn-wrap">
     <a
-      class="icon-btn group inline-flex cursor-pointer items-center gap-4 rounded-3xl bg-blue px-7 py-3 text-base text-white transition-all hover:bg-white hover:text-blue"
       :href="link"
+      class="relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-medium transition-all rounded-full bg-blue hover:bg-white group"
     >
-      {{ buttonText }}
-      <arrow
-        class="h-7 w-6 fill-white transition-all group-hover:ml-1 group-hover:fill-blue"
-      ></arrow>
+      <span
+        class="absolute bg-blue inset-0 border-0 group-hover:border-[25px] ease-linear duration-100 transition-all border-white rounded-full"
+      ></span>
+      <span
+        class="relative flex items-center w-full gap-4 text-left text-white transition-colors duration-200 ease-in-out rounded-full group-hover:text-blue group-hover:bg-white"
+        >{{ buttonText }}
+        <arrow
+          class="w-6 transition-all h-7 fill-white group-hover:ml-1 group-hover:fill-blue"
+        ></arrow>
+      </span>
     </a>
   </div>
 </template>
