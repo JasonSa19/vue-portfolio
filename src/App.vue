237e28ede330d12179fec23f6a22fd5a84fff7f3
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-08-23 17:04:00 +0200

    Init Footer Component Base

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Intro from "./components/Intro.vue";
 import About from "./components/About.vue";
 import Attributes from "./components/Attributes.vue";
 import Areas from "./components/Areas.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import Areas from "./components/Areas.vue";
   <About />
   <Attributes />
   <Areas />
+  <Footer />
 </template>
 
 <style lang="scss">
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+// const linkMailaddress = ref("jason@sauerwald.info");
+// const linkLinkedin = ref("https://www.linkedin.com/in/jasonsauerwald/");
+// const linkGithub = ref("https://github.com/JasonSa19/");
+// const linkInstagram = ref("https://www.instagram.com/jason_sa_/");
+
+// import mailbox from "./svg/mailbox.vue";
+// import github from "./svg/github.vue";
+// import instagram from "./svg/instagram.vue";
+// import linkedin from "./svg/linkedin.vue";
+</script>
+
+<template>foo</template>
