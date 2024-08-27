3ac6c35f3ec6564ca16f2b42f03580c7f849b3e3
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-08-27 18:19:18 +0200

    Mode toggle dummy

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router/index.js"; // Import the router configuration
 import "./assets/scss/style.scss";
 
 document.addEventListener("DOMContentLoaded", function () {
+  // scroll-animation
   const fadeUpElements = document.querySelectorAll(".anim");
 
   const observerOptions = {
@@ -21,6 +22,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
   fadeUpElements.forEach((element) => {
     observer.observe(element);
+  });
+
+  // dark-mode switch
+  const toggleButton = document.getElementById("theme-toggle");
+  const htmlElement = document.documentElement;
+
+  // Check for existing dark mode preference
+  if (
+    localStorage.getItem("theme") === "dark" ||
+    (!localStorage.getItem("theme") &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    htmlElement.classList.add("dark");
+  } else {
+    htmlElement.classList.remove("dark");
+  }
+
+  // Toggle dark mode on button click
+  toggleButton.addEventListener("click", () => {
+    htmlElement.classList.toggle("dark");
+    if (htmlElement.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
   });
 });
 
