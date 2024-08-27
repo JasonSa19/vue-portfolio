9c7f0e1d6e7515ddb976484138694cda88bc7019
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-08-27 15:10:41 +0200

    Import analytcs

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js"; // Import the router configuration
 import "./assets/scss/style.scss";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 createApp(App).use(router).mount("#app");
