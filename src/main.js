435b746c1fd6e3542d88ca89d40d7f21fb5b8c72
Jason Sauerwald <115042184+JasonSa19@users.noreply.github.com>
2024-08-27 17:44:06 +0200

    Simple scroll-animation

--- src/main.js
+++ src/main.js
@@ -3,4 +3,26 @@ import App from "./App.vue";
 import router from "./router/index.js"; // Import the router configuration
 import "./assets/scss/style.scss";
 
+// scroll-animation
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeUpElements = document.querySelectorAll(".anim");
+
+  const observerOptions = {
+    threshold: 0.1,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  fadeUpElements.forEach((element) => {
+    observer.observe(element);
+  });
+});
+
 createApp(App).use(router).mount("#app");
